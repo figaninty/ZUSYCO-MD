cae78f0c873d0520d70bf778fd49e04688325715
figaninty <165603706+figaninty@users.noreply.github.com>
2024-04-01 00:33:13 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,10 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "254723616685",
+  SUDO_NUMBERS : "254723616685,254723616685",
+  OWNER_NAME: "『 𝙼𝚁juma 』",
+  BOT_NAME : "jumatechn",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
